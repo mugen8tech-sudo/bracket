de4412f705265433cf403caa9a99b87d7fee17e1
mugenzen <mugen8.tech@gmail.com>
2025-10-12 23:30:00 +0700

    Create page.tsx

---
+++ app/(private)/bank_mutations/page.tsx
@@ -0,0 +1,8 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+import BankMutationsTable from "@/components/bank-mutations-table";
+
+export default function BankMutationsPage() {
+  return <BankMutationsTable />;
+}
