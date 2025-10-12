4e0562b72ac8c6a7823cccc6390f1d672d8b15d7
mugenzen <mugen8.tech@gmail.com>
2025-10-12 18:01:09 +0700

    Create page.tsx

---
+++ app/(private)/page.tsx
@@ -0,0 +1,8 @@
+export default function Landing() {
+  return (
+    <div className="rounded border bg-white p-6">
+      <h1 className="text-xl font-semibold mb-2">Login Sukses.</h1>
+      </p>
+    </div>
+  );
+}
