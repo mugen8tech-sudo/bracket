45f2f42ee9846a2923eb037966e2caaf005268d1
mugenzen <mugen8.tech@gmail.com>
2025-10-13 13:57:23 +0700

    Update page.tsx

--- app/(private)/credit_mutations/page.tsx
+++ app/(private)/credit_mutations/page.tsx
@@ -1,7 +1,7 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-import BankMutationsTable from "@/components/bank-mutations-table";
+import BankMutationsTable from "@/components/credit-mutations-table";
 
 export default function CreditMutationsTable() {
   return <CreditMutationsTable />;
