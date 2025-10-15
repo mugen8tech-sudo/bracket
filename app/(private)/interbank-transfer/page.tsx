e24320ac8b1e0e6ab43bb6697fff336f69fda96e
mugenzen <mugen8.tech@gmail.com>
2025-10-15 13:18:26 +0700

    Delete app/(private)/interbank-transfer directory

--- app/(private)/interbank-transfer/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-import InterbankTransfersTable from "@/components/interbank-transfers-table";
-
-export default function InterbankTransferPage() {
-  return <InterbankTransfersTable />;
-}
