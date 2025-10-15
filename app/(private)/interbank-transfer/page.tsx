7505d619b2b11928c5103abac20f3e55a279e195
mugenzen <mugen8.tech@gmail.com>
2025-10-15 13:06:23 +0700

    Update page.tsx

--- app/(private)/interbank-transfer/page.tsx
+++ app/(private)/interbank-transfer/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import InterbankTransfersTable from "@/components/interbank-transfers-table";
 
 export default function InterbankTransferPage() {
