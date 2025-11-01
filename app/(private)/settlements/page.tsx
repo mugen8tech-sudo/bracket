2af375d4d3fde43afd4c6b0d92344e600816514e
mugenzen <mugen8.tech@gmail.com>
2025-11-01 23:42:11 +0700

    Create page.tsx

---
+++ app/(private)/settlements/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SettlementsTable from "@/components/settlements-table";
+
+export default function Page() {
+  return (
+    <div className="space-y-3">
+      <SettlementsTable />
+    </div>
+  );
+}
