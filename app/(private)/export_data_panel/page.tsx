dbc414bd5f039469fc4dab8abd689a41f9fe2b8b
mugenzen <mugen8.tech@gmail.com>
2025-12-26 21:36:12 +0700

    Create page.tsx

---
+++ app/(private)/export_data_panel/page.tsx
@@ -0,0 +1,8 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+import ExportDataPanelShell from "@/components/export-data-panel-shell";
+
+export default function ExportDataPanelPage() {
+  return <ExportDataPanelShell />;
+}
