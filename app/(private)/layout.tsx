ad21f8186a10c5ad21d5ac3f78c67f3cd8e110c5
mugenzen <mugen8.tech@gmail.com>
2026-01-02 15:10:36 +0700

    Update layout.tsx

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -3,14 +3,16 @@ import Sidebar from "@/components/sidebar";
 
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Header />
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 px-4 py-6 bg-gray-50 min-h-[calc(100vh-56px)]">
-          {children}
-        </main>
+    <div className="min-h-screen overflow-x-auto bg-gray-50">
+      <div className="w-fit min-w-full">
+        <Header />
+        <div className="flex">
+          <Sidebar />
+          <main className="flex-1 px-4 py-6 min-h-[calc(100vh-56px)]">
+            {children}
+          </main>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
